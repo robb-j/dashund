6d5fde4038a17c2bffdc3aacc59f18bc459ad001
Rob Anderson <rob.anderson@ncl.ac.uk>
2019-06-21 16:42:56 +0100

    :alembic: Start temporary server

--- src/utils/temporary-server.js
+++ src/utils/temporary-server.js
@@ -1,1 +1,25 @@
-// ...
+const express = require('express')
+const stoppable = require('stoppable')
+const { createServer } = require('http')
+const { promisify } = require('util')
+
+async function runTemporaryServer(port, setup) {
+  let app = express()
+  let server = stoppable(createServer(app))
+
+  return new Promise(async (resolve, reject) => {
+    await startServer(app, port)
+
+    const close = promisify(server.close).bind(server)
+
+    await setup(app, close)
+  })
+}
+
+function startServer(app, port) {
+  return new Promise((resolve, reject) => {
+    app.listen(port, resolve)
+  })
+}
+
+module.exports = { runTemporaryServer }
